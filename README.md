2dbf26ffb186d5242cd643c5f7a65a7dbd94e01b
klgunarathne <k.l.gunarathne1988@gmail.com>
2021-06-05 13:49:17 +0530

    read qr code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Track-vehicles
-Python 3.9
+Python 3.8
 Use this commands to run application
 1. open command promt on the file location
 2. type ./venv/Scripts/activate
--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 print("Hello world")
-import cv2
+import cv2;
+from pyzbar import pyzbar
+
+
+img = cv2.imread("qr code.png", cv2.IMREAD_COLOR)
+
+barcodes = pyzbar.decode(img)
+
+# loop over the detected barcodes
+for barcode in barcodes:
+	(x, y, w, h) = barcode.rect
+	cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	barcodeData = barcode.data.decode("utf-8")
+	barcodeType = barcode.type
+	text = "{} ({})".format(barcodeData, barcodeType)
+	cv2.putText(img, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+		0.5, (0, 0, 255), 2)
+	print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+ 
+cv2.imshow("qr code", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
